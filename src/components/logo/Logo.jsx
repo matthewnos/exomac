b1bcf594afd543c61993b8b878871bd5ac8b4ebe
Matthew Nos <mnos@cloudingenuity.com>
2023-02-06 18:47:36 -0600

    Changed LogoFile

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -7,7 +7,7 @@ const Logo = ({image}) => {
     return(
         <div className="header-logo">
             <Link to={process.env.PUBLIC_URL + "/"}>
-                <img className="dark-logo" src={process.env.PUBLIC_URL + image} alt="Agency Logo" />
+                <img className="dark-logo" src={process.env.PUBLIC_URL + "/assets/logo.png"} alt="Agency Logo" />
             </Link>
         </div>
     )
@@ -18,3 +18,25 @@ Logo.propTypes = {
 };
 
 export default Logo;
+
+//Original Code
+// import PropTypes from "prop-types";
+// import React from 'react';
+// import {Link} from "react-router-dom";
+
+
+// const Logo = ({image}) => {
+//     return(
+//         <div className="header-logo">
+//             <Link to={process.env.PUBLIC_URL + "/"}>
+//                 <img className="dark-logo" src={process.env.PUBLIC_URL + image} alt="Agency Logo" />
+//             </Link>
+//         </div>
+//     )
+// }
+
+// Logo.propTypes = {
+//     image: PropTypes.string
+// };
+
+// export default Logo;
