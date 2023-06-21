fa2a5cea60a09e0672b2383aca2a828844f603be
Andrew F <afairchild@cloudingenuity.com>
2023-06-21 15:37:17 -0500

    margin changes

--- src/container/About/AboutFour.js
+++ src/container/About/AboutFour.js
@@ -4,7 +4,7 @@ import Tilt from 'react-parallax-tilt';
 import Parallax from 'parallax-js';
 import CountUp from 'react-countup';
 import VisibilitySensor from "react-visibility-sensor";
-import '../../assets/css/style.css'
+import '../../assets/css/style.css';
 
 const AboutFour = () => {
     const [didViewCountUp, setDidViewCountUp] = useState(false);
@@ -31,7 +31,7 @@ const AboutFour = () => {
                 <div className="row">
 
                     <div className="col-xl-6 col-lg-6 col-12" data-aos="fade-up">
-                        <div className="about-content-area mt-0 mb-md-10 mb-10">
+                        <div className="about-content-area mt-0 mb-md-0 mb-0">
 
                             <p className="about-paragraph">
                                 Customers turn to Cloud Ingenuity as we bring Fortune 100 strategy to the masses to drive business transformation. Our comprehensive services span 5 key tenants: Multi Cloud, Cyber Security, Network Modernization, Collaboration, and Enterprise Visibility. Our team helps customers determine current challenges, and we partner to build a plan for implementing the "best of breed" solutions needed to achieve successful business outcomes. Our greatest thrill is when collaboration with our team leads to measured success within a customer's business. Whether we’ve increased ROI, secured their environment, or improved time to market; our customers find great satisfaction in knowing that Cloud Ingenuity is a partner they can trust. We created this company with the sole focus of 100% customer satisfaction and driving business outcomes.
