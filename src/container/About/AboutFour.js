71d10e2a3b5c45da9d0868cb16e5a1da446343f8
Andrew F <afairchild@cloudingenuity.com>
2023-06-22 10:31:25 -0500

    more changes and pictures

--- src/container/About/AboutFour.js
+++ src/container/About/AboutFour.js
@@ -29,10 +29,8 @@ const AboutFour = () => {
         <div className="section section-padding-top about-section-padding-bottom-200">
             <div className="container">
                 <div className="row">
-
                     <div className="col-xl-6 col-lg-6 col-12" data-aos="fade-up">
                         <div className="about-content-area mt-0 mb-md-0 mb-0">
-
                             <p className="about-paragraph mr-0">
                                 Customers turn to Cloud Ingenuity as we bring Fortune 100 strategy to the masses to drive business transformation. Our comprehensive services span 5 key tenants: Multi Cloud, Cyber Security, Network Modernization, Collaboration, and Enterprise Visibility. Our team helps customers determine current challenges, and we partner to build a plan for implementing the "best of breed" solutions needed to achieve successful business outcomes. Our greatest thrill is when collaboration with our team leads to measured success within a customer's business. Whether we’ve increased ROI, secured their environment, or improved time to market; our customers find great satisfaction in knowing that Cloud Ingenuity is a partner they can trust. We created this company with the sole focus of 100% customer satisfaction and driving business outcomes.
                             </p>
@@ -44,13 +42,10 @@ const AboutFour = () => {
                             <p className="about-paragraph mr-0">
                                 With the right people and the right partners, Cloud Ingenuity is able to focus on solutions to help IT cut costs, increase reliability, ease the burden of management, maximize flexibility, mitigate risk, and improve service. We require our teams to be focused so they can help you get from the high-level architecture of a solution all the way down to the technical configuration and implementation of IT solutions and products.
                             </p>
-
                         </div>
-                    </div>
-                    <div className="about-content-area mt-0 mb-md-0 mb-0">
-                    <p className="about-Large">
-                    WE ENGAGE AND PARTNER WITH YOU UNTIL BUSINESS VALUE IS DELIVERED.
-                            </p>
+                        <p className="about-Large">
+                            WE ENGAGE AND PARTNER WITH YOU UNTIL BUSINESS VALUE IS DELIVERED.
+                        </p>
                     </div>
                     <div className="col-xl-6 col-lg-6 col-12" data-aos="fade-up" data-aos-delay="300">
                         <div className="about-image-area about-shape-animation right-0 me-0">
