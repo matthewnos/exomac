dcf95fe5e7bbb6344280219a45cabf27ab6cf036
Andrew F <afairchild@cloudingenuity.com>
2023-06-23 09:17:33 -0500

    added video and some other components to the Services tab

--- src/container/service/ServiceIconBoxTwo.js
+++ src/container/service/ServiceIconBoxTwo.js
@@ -1,59 +1,77 @@
 import PropTypes from "prop-types";
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 import IconBoxData from '../../data/iconBox/icon-box.json';
 import IconBox from '../../components/IconBox/IconBox.jsx';
 import SectionTitle from '../../components/SectionTitles/SectionTitle';
 import Parallax from 'parallax-js';
 
 const ServiceIconBoxTwo = ({ classOption }) => {
-    const sceneEl = useRef(null);
-    useEffect(() => {
-        const parallaxInstance = new Parallax(sceneEl.current, {
-        relativeInput: true,
-        })
-        
-        parallaxInstance.enable();
+  const sceneEl = useRef(null);
 
-        return () => parallaxInstance.disable();
+  useEffect(() => {
+    const parallaxInstance = new Parallax(sceneEl.current, {
+      relativeInput: true,
+    });
 
-    }, [])
-    return (
-        <div className={`section section-padding-t90 section-padding-bottom ${classOption}`}>
-            <div className="container">
+    parallaxInstance.enable();
 
-                <SectionTitle
-                    headingOption="fz-32"
-                    title="Services that create identities, build brands, and get results"
-                    subTitle="We produce beautifully crafted creative solutions that transcend business goals.
-                    We provide the exceptional service we’d want to experience ourselves!"
-                />
+    return () => parallaxInstance.disable();
+  }, []);
 
-                <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-2 row-cols-1 mb-n6 icon-box-shape-animation">
-
-                    {IconBoxData && IconBoxData.map((single, key) => {
-                        return(
-                            <div key={key} className="col mb-6" data-aos="fade-up" data-aos-delay="300">
-                                <IconBox classOption="box-border" data={single} key={key} />
-                            </div>
-                        ); 
-                    })}
-
-                    <div className="shape shape-1" id="scene" ref={sceneEl}>
-                        <span data-depth="1"><img src={process.env.PUBLIC_URL + "/images/shape-animation/video-shape-1.png"} alt="" /></span>
-                    </div>
-
-                </div>
-
+  return (
+    <div className={`section section-padding-t90 section-padding-bottom ${classOption}`}>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6" data-aos="fade-up">
+            <img src="/images/services/Location.png" alt="" style={{ maxHeight: '400px', height: '100%' }} />
+          </div>
+          <div className="col-lg-6" data-aos="fade-up">
+            <div style={{ width: '100%' }}>
+              <SectionTitle
+                headingOption="fz-32"
+                title="Services that create identities, build brands, and get results"
+                subTitle="Cloud Ingenuity's Application for Location Services"
+              />
+              <video controls style={{ width: '100%' }}>
+                <source src="/videos/LocationVideo.mp4" type="video/mp4" />
+              </video>
             </div>
+          </div>
         </div>
-    )
+        <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-2 row-cols-1 mb-n6 icon-box-shape-animation" style={{ marginTop: '20px' }}>
+          {IconBoxData &&
+            IconBoxData.map((single, key) => {
+              return (
+                <div
+                  key={key}
+                  className="col mb-6"
+                  data-aos="fade-up"
+                  data-aos-delay="300"
+                >
+                  <IconBox classOption="box-border" data={single} key={key} />
+                </div>
+              );
+            })}
+          <div className="shape shape-1" id="scene" ref={sceneEl}>
+            <span data-depth="1">
+              <img
+                src={process.env.PUBLIC_URL + "/images/shape-animation/video-shape-1.png"}
+                alt=""
+              />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ServiceIconBoxTwo.propTypes = {
-    classOption: PropTypes.string
-};
-ServiceIconBoxTwo.defaultProps = {
-    classOption: "section section-padding-t90 section-padding-bottom"
+  classOption: PropTypes.string
 };
 
-export default ServiceIconBoxTwo
+ServiceIconBoxTwo.defaultProps = {
+  classOption: "section section-padding-t90 section-padding-bottom"
+};
+
+export default ServiceIconBoxTwo;
